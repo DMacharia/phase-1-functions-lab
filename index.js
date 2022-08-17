c62728ee865441ab00d5ae1fb76674e1c55a8542
Daniel Macharia <danielhuios@gmail.com>
2022-08-17 17:42:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+  const scuberHQ = 42;
+  return Math.abs(start - scuberHQ);
+}
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet(start1) {
+  //1 block = 264feet
+  return distanceFromHqInBlocks(start1) * 264;
+}
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
+
+function calculatesFarePrice(start, destination) {
+  let feetTravelled = distanceTravelledInFeet(start, destination);
+  if (feetTravelled <= 400) {
+    return 0;
+  } else if (feetTravelled < 2000) {
+    return (feetTravelled - 400) * 0.02;
+  } else if (feetTravelled > 2500) {
+    return "cannot travel that far";
+  } else {
+    return 25;
+  }
+}
+
+calculatesFarePrice(43, 44);
+calculatesFarePrice(34, 32);
+calculatesFarePrice(50, 58);
+calculatesFarePrice(34, 24);
